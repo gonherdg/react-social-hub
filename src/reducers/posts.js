337d5e23026847d31e9626fc7cb79b7ecd43f8f4
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2022-08-26 04:18:17 -0300

    refactor: created constants to avoid strings inside code

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,15 +1,16 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+
 export default (state = [], action) => {
     switch (action.type) {
-        case "FETCH_ALL":
+        case FETCH_ALL:
             return action.payload;
-        case "CREATE":
+        case CREATE:
             return [...state, action.payload];
-        case "UPDATE":
-        case "LIKE":
+        case UPDATE:
             return state.map((post) =>
                 post._id === action.payload._id ? action.payload : post
             );
-        case "DELETE":
+        case DELETE:
             return state.filter((post) => post._id !== action.payload);
 
         default:
