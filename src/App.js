c35916eb1c697ed4dd389a2d19373311f8ac731b
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2022-12-02 19:09:16 -0300

    refactor: moved the AppBar to a new component NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
 import { getPosts } from "./actions/posts";
 import Posts from "./components/Posts/Posts.js";
 import Form from "./components/Form/Form.js";
-import memories from "./images/picnic.png";
 import useStyles from "./styles.js";
 import "./index.css";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -20,22 +20,7 @@ const App = () => {
 
     return (
         <Container maxwidth="lg">
-            <AppBar
-                className={classes.appBar}
-                position="static"
-                color="inherit">
-                <Typography
-                    className={classes.heading}
-                    variant="h2"
-                    align="center">
-                    Memories
-                </Typography>
-                <img
-                    className={classes.image}
-                    src={memories}
-                    alt="memories"
-                    height="60"></img>
-            </AppBar>
+            <Navbar />
             <Grow in>
                 <Container>
                     <Grid
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { AppBar, Typography } from "@material-ui/core";
+
+import memories from "../../images/picnic.png";
+import useStyles from "./styles";
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <Typography className={classes.heading} variant="h2" align="center">
+                Memories
+            </Typography>
+            <img
+                className={classes.image}
+                src={memories}
+                alt="memories"
+                height="60"></img>
+        </AppBar>
+    );
+};
+
+export default Navbar;
