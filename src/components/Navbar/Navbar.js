010e3108a6ed7d5930b138726d6fa9813ae487e7
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2022-12-02 22:18:57 -0300

    feat: created Home and Auth to separate code ready for auth screen

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { AppBar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { AppBar, Typography, Toolbar, Button, Avatar } from "@material-ui/core";
 
 import memories from "../../images/picnic.png";
 import useStyles from "./styles";
@@ -7,16 +8,49 @@ import useStyles from "./styles";
 const Navbar = () => {
     const classes = useStyles();
 
+    const user = null;
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <Typography className={classes.heading} variant="h2" align="center">
-                Memories
-            </Typography>
-            <img
-                className={classes.image}
-                src={memories}
-                alt="memories"
-                height="60"></img>
+            <div className={classes.brandContainer}>
+                <Typography
+                    component={Link}
+                    to="/"
+                    className={classes.heading}
+                    variant="h2"
+                    align="center">
+                    Memories
+                </Typography>
+                <img
+                    className={classes.image}
+                    src={memories}
+                    alt="memories"
+                    height="60"></img>
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar
+                            className={classes.purple}
+                            alt={user.result.name}
+                            src={user.result.imageUrl}>
+                            {user.result.name.charAt(0)}
+                        </Avatar>
+                        <Button
+                            variant="contained"
+                            className={classes.logout}
+                            color="secondary">
+                            Logout
+                        </Button>
+                    </div>
+                ) : (
+                    <Button
+                        component={Link}
+                        to="/auth"
+                        variant="contained"
+                        color="primary"></Button>
+                )}
+            </Toolbar>
         </AppBar>
     );
 };
