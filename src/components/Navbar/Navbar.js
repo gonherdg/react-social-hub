bbd85a71b1e644a885696fa3068179874be6956e
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2023-03-10 01:54:31 -0300

    feat: logout on token expiration

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { AppBar, Typography, Toolbar, Button, Avatar } from "@material-ui/core";
 import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
 import memories from "../../images/picnic.png";
 import useStyles from "./styles";
 
@@ -23,6 +24,14 @@ const Navbar = () => {
     };
 
     useEffect(() => {
+        const token = user?.token;
+        console.log("user token: ", token, " ", user);
+        if (token) {
+            const decodedToken = decode(token);
+
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
+
         const profile = JSON.parse(localStorage.getItem("profile"));
         if (profile?.result !== undefined) {
             setUser(profile.result);
