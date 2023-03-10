7aa5720701612e3ce2760e769ea9e617438d687f
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2023-03-09 23:37:17 -0300

    feat: hidden delete and options button when not property of loged user

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -23,7 +23,8 @@ const Post = ({ post, setCurrentId }) => {
     const dispatch = useDispatch();
     const user = JSON.parse(localStorage.getItem("profile"));
 
-    console.log(post.likes.length);
+    //console.log("post.likes.length: ", post.likes.length);
+    //console.log("post: ", post);
 
     const Likes = () => {
         if (post.likes.length > 0) {
@@ -69,14 +70,17 @@ const Post = ({ post, setCurrentId }) => {
                     {moment(post.createdAt).fromNow()}
                 </Typography>
             </div>
-            <div className={classes.overlay2}>
-                <Button
-                    style={{ color: "white" }}
-                    size="small"
-                    onClick={() => setCurrentId(post._id)}>
-                    <MoreHorizIcon fontSize="medium"></MoreHorizIcon>
-                </Button>
-            </div>
+            {(user?.googleId === post?.creator ||
+                user?.result?._id === post?.creator) && (
+                <div className={classes.overlay2}>
+                    <Button
+                        style={{ color: "white" }}
+                        size="small"
+                        onClick={() => setCurrentId(post._id)}>
+                        <MoreHorizIcon fontSize="medium"></MoreHorizIcon>
+                    </Button>
+                </div>
+            )}
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">
                     {post.tags.map((tag) => `#${tag} `)}
@@ -98,13 +102,16 @@ const Post = ({ post, setCurrentId }) => {
                     onClick={() => dispatch(likePost(post._id))}>
                     <Likes />
                 </Button>
-                <Button
-                    size="small"
-                    color="primary"
-                    onClick={() => dispatch(deletePost(post._id))}>
-                    <DeleteIcon fontSize="small"></DeleteIcon>
-                    Delete
-                </Button>
+                {(user?.googleId === post?.creator ||
+                    user?.result?._id === post?.creator) && (
+                    <Button
+                        size="small"
+                        color="primary"
+                        onClick={() => dispatch(deletePost(post._id))}>
+                        <DeleteIcon fontSize="small"></DeleteIcon>
+                        Delete
+                    </Button>
+                )}
             </CardActions>
         </Card>
     );
