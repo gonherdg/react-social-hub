1a864e9c3fa8ff5a30d1b822532f2adfc20f0d63
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2023-06-15 01:54:30 -0300

    feat: using AWS Lambda address as baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });  // Local development
+// https://1ljmxxotee.execute-api.us-east-1.amazonaws.com/      // AWS Lambda serverless
+// http://localhost:9000/.netlify/functions/api         // Netlify Lambda serverless
+const API = axios.create({
+    baseURL: "https://1ljmxxotee.execute-api.us-east-1.amazonaws.com/",
+});
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
