d79b7122ccaaa6a80ed708134fdce7174fc087d9
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2023-07-05 23:09:23 -0300

    style: responsive footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,15 +2,19 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
     background: {
-        backgroundColor: theme.palette.grey[700],
-        minHeight: "400px",
-        marginTop: "160px",
+        backgroundColor: theme.palette.grey[800],
+        minHeight: "340px",
+        marginTop: "100px",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         boxShadow:
             "0px -3px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
+        [theme.breakpoints.down("md")]: {
+            minHeight: "220px",
+            marginTop: "60px",
+        },
     },
     text: {
         fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
